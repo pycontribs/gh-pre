1b55446344f77de95617051727dbde5551b49b90
Sorin Sbarnea <ssbarnea@redhat.com>
2024-03-25 16:43:20 +0000

    Add ability to report dependabot alerts (#9)

--- src/gh_pre/__main__.py
+++ src/gh_pre/__main__.py
@@ -16,20 +16,32 @@ import typer
 from typer_config.decorators import use_yaml_config
 
 
-app = typer.Typer()
+class TyperApp(typer.Typer):
+    """Our App."""
+
+    repos: list[str]
 
 
-@app.command()
+app = TyperApp()
+console = Console()
+
+
+@app.callback(invoke_without_command=True)
 @use_yaml_config(
     default_value=os.path.expanduser("~/pre.yml"),
     param_help="Configuration file (~/pre.yml).",
 )
-def main(repos: Annotated[Optional[list[str]], typer.Option()] = None) -> None:
-    """Pre helps you chain releases on github."""
+def default(repos: Annotated[Optional[list[str]], typer.Option()] = None) -> None:
+    """Implicit entry point."""
     if repos is None:
         repos = []
-    console = Console()
-    for repo in repos:
+    app.repos = repos
+
+
+@app.command()
+def main() -> None:
+    """Pre helps you chain releases on github."""
+    for repo in app.repos:
         repo_link = f"[markdown.link][link=https://github.com/{repo}]{repo}[/][/]"
         result = run(
             f'gh api repos/{repo}/releases --jq "[.[] | select(.draft)"]',
@@ -61,6 +73,51 @@ def main(repos: Annotated[Optional[list[str]], typer.Option()] = None) -> None:
             )
             console.print(msg, highlight=False, end="")
             console.print(md, style="dim")
+
+
+@app.command()
+def prs() -> None:
+    """List pending pull-request."""
+    # for user in TEAM:
+    # --review-requested=@{user}
+    # --owner=ansible --owner=ansible-community
+    cmd = (
+        "GH_PAGER= gh search prs --draft=false --state=open --limit=100 --sort=updated"
+    )
+    cmd += "".join(f" --repo={repo}" for repo in app.repos)
+    cmd += (
+        " --template '{{range .}}{{tablerow .repository.nameWithOwner (timeago .updatedAt) "
+        '.title (hyperlink .url (printf "#%v" .number) ) }}{{end}}{{tablerender}}\' '
+        "--json title,url,repository,updatedAt,number"
+    )
+    console.print(f"[dim]{cmd}[/]", highlight=False)
+    os.system(cmd)
+
+
+@app.command()
+def alerts() -> None:
+    """List open alerts."""
+    for repo in app.repos:
+        cmd = "GH_PAGER= gh "
+        cmd += f"api /repos/{repo}/dependabot/alerts"
+        cmd += " --jq='.[] | select(.state!=\"fixed\") | .html_url'"
+        result = run(
+            cmd,
+            text=True,
+            shell=True,
+            capture_output=True,
+            check=False,
+        )
+        if result.returncode:
+            console.print(
+                f"[dim]{cmd}[/dim] failed with {result.returncode}\n"
+                f"{result.stdout}\n\n{result.stderr}"
+            )
+        else:
+            if result.stdout:
+                console.print(result.stdout)
+            if result.stderr:
+                console.print(result.stderr)
 
 
 if __name__ == "__main__":
